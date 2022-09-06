eb0e16aa11b8ecfa73628108b785bbe72f1ff8f2
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-06 09:21:53 -0700

    Task 0

---
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    matrix_T = []
+    for j in range(columns):
+        row = []
+        for i in range(rows):
+           row.append(matrix[i][j] ** 2)
+        matrix_T.append(row)
+
+    return matrix_T

974ca2b3fb4cb2e70c53e2d99b9ed45ea004a693
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-06 09:34:44 -0700

    whoa nelly, i need to learn python for real

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        rows = len(matrix)
-        columns = len(matrix[0])
-
-        matrix_T = []
-        for j in range(columns):
-            row = []
-            for i in range(rows):
-                row.append(matrix[i][j] ** 2)
-            matrix_T.append(row)
-
-        return matrix_T
+    return((int**2 for int in row) for row in matrix)

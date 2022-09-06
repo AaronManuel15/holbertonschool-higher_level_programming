780dbece16252dacdeaafe9e7d5955e0365555eb
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-06 09:26:42 -0700

    if guard

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    rows = len(matrix)
-    columns = len(matrix[0])
+    if matrix:
+        rows = len(matrix)
+        columns = len(matrix[0])
 
-    matrix_T = []
-    for j in range(columns):
-        row = []
-        for i in range(rows):
-            row.append(matrix[i][j] ** 2)
-        matrix_T.append(row)
+        matrix_T = []
+        for j in range(columns):
+            row = []
+            for i in range(rows):
+                row.append(matrix[i][j] ** 2)
+            matrix_T.append(row)
 
-    return matrix_T
+        return matrix_T

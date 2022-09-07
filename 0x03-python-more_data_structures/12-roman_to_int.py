329c948908070d45cfc577254add03f50f8ce023
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-06 18:25:46 -0700

    Changed indentation for pep8

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
     count = 0
     lastDigit = 0
-    romanNumeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romanNumeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
     for x in roman_string:
         if lastDigit != 0 and lastDigit < romanNumeral[x]:
             count -= lastDigit

5c46f8473764b19dbbb0352132b4b90a13edcf7f
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-07 11:22:07 -0700

    Task 0 and readme added

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""I'm unsure what this part is for"""
+
+
+class Square:
+    """This is the Square"""
+    def __init__(self, size):
+        """Adding private size attribute"""
+        self.__size = size

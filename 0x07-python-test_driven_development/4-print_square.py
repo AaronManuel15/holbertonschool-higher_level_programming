64bf9a0c7510955e825daafcd76e7e8bbf93afc7
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-10 18:30:47 -0700

    Task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Project 0x05 - Test driven development
+    Task 3
+    Prints out a square built by #
+"""
+
+
+def print_square(size):
+    """
+        Prints out the square defined by size
+
+        Args:
+            size: _description_
+    """
+    if type(size) not in [int]:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+            print("")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print("")

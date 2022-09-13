ba605c3f62d485eb16fb06238893c787912a869d
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-12 21:11:16 -0700

    Task 5 with comments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,15 +7,23 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_valid(self):
+        """Testing positive int"""
         self.assertEqual(max_integer([1, 2, 3]), 3)
+        """Testing negatives"""
         self.assertEqual(max_integer([-1, -3, -5]), -1)
+        """Testing single value"""
         self.assertEqual(max_integer([4]), 4)
+        """Testing variable"""
         self.assertEqual(max_integer(a), a)
+        """Testing string"""
         self.assertEqual(max_integer("string", "s"))
 
     def test_invalid(self):
+        """Testing mixed values"""
         self.assertRaises(TypeError, max_integer, [5, "string", 10])
+        """Testing no lists"""
         self.assertRaises(TypeError, max_integer, 1)
+        """Testing None"""
         self.assertRaises(max_integer([]), None)
 
 

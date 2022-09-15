9463828c7aae1f66e4876ac4fb7457a838a3aa21
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 11:13:03 -0700

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Project 0x07. Python - Inheritance
+    Task 1
+"""
+
+
+class MyList(list):
+    """Class that inherits from list"""
+
+    def print_sorted(self):
+        """ Function to print the list, but sorted in ascending"""
+        print(sorted(self))

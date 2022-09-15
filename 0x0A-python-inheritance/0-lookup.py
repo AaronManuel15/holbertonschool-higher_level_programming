f97ba9f203999a0f4257539f9e855248cfce18ff
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 08:54:12 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Project 0x07. Python - Inheritance
+    Task 0
+"""
+def lookup(obj):
+    """ Function to return list of attributes and methods of obj"""
+    return (dir(obj))

a4978e0d20a5ad954b4c941707eb0b688d447657
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 15:42:12 -0700

    Spaced out my notes for doc test

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,13 +5,16 @@
 
 
 class BaseGeometry:
-    """Class to contain geometric functions"""
+    """Class to contain geometric functions
+    """
     def area(self):
-        """area function with nothing currently implemented"""
+        """area function with nothing currently implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates types and values of geometric attributes"""
+        """validates types and values of geometric attributes
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:

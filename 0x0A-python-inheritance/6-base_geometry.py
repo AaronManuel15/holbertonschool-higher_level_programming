3d6e4649782adc1ecd0ec316471c8cd9830ecaa0
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 13:15:30 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Project 0x07. Python - Inheritance
+    Task 6
+"""
+
+
+class BaseGeometry:
+    """Class to contain geometric functions"""
+    def area(self):
+        raise Exception("area() is not implemented")

3b245f1903b7c1bda9dbf11b1981a8c36fff3bd1
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 12:49:00 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Project 0x07. Python - Inheritance
+    Task 3
+"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is an instance of a_class or it's parent class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

3333061f28f8e78534ec04cf9c0afcd1b49ecbe1
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-15 12:49:58 -0700

    Task 3 - Changed prototye

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Checks if obj is an instance of a_class or it's parent class"""
     if isinstance(obj, a_class):
         return True

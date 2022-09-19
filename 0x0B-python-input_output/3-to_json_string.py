d695fe37e45dfe56677ab0cdb1ac8b86b54b455a
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-19 15:23:38 -0700

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Project 0x08. Python - Input/Output
+    Task 3
+"""
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object (string):
+    """
+    return json.dumps(my_obj)

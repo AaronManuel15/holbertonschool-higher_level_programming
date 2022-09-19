5a45a98a57d846978f1d9c15e001da6969f9f4cf
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-19 14:57:31 -0700

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Project 0x08. Python - Input/Output
+    Task 0
+"""
+
+
+def read_file(filename=""):
+    """Program that eads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding="utf=8") as file:
+        fileText = fileText.read()
+        print(fileText, end="")

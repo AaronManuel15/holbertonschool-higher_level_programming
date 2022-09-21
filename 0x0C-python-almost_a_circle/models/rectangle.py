4834771d07c1d6027b758dfc1687bd24899c6860
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-21 13:53:53 -0700

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Project 0x0C. Python - Almost a circle
+    Task 1
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Defines a rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Getter for width
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x
+        """
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y
+        """
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y
+        """
+        self.__y = value

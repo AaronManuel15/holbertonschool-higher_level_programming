b652167113f13c0b096d74925a1497c4d3305dac
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-21 18:26:59 -0700

    Task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,3 +46,8 @@ class Square(Rectangle):
         if kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Square
+        """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}

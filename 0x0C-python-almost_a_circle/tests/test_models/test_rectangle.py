73b5c038c04e43dfb7e5e06236a3c110355c936f
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-23 13:17:59 -0700

    Updated basic format for other test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Unit testing for Rectangle class"""
 import unittest
+
+
+class TestRectangle(unittest.TestCase):
+    """class for testing Rectangle class"""
+    pass
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Unit testing for Square class"""
 import unittest
+
+
+class TestSquare(unittest.TestCase):
+    """class for testing Square class"""
+    pass

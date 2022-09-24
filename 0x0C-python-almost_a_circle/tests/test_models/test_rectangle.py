8493f856f472b424bc27f7b0fc27dfe46629d722
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-23 20:01:42 -0700

    document checks added

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """Unit testing for Rectangle class"""
 import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestRectangle(unittest.TestCase):
     """class for testing Rectangle class"""
-    pass
+    
+    def test_docstrings(self):
+        self.assertIsNotNone(__import__('models').rectangle.__doc__)
+        self.assertIsNotNone(Rectangle.__doc__)
+        self.assertIsNotNone(Rectangle.__init__.__doc__)
+        self.assertIsNotNone(Rectangle.width.__doc__)
+        self.assertIsNotNone(Rectangle.height.__doc__)
+        self.assertIsNotNone(Rectangle.x.__doc__)
+        self.assertIsNotNone(Rectangle.y.__doc__)
+        self.assertIsNotNone(Rectangle.area.__doc__)
+        self.assertIsNotNone(Rectangle.display.__doc__)
+        self.assertIsNotNone(Rectangle.__str__.__doc__)
+        self.assertIsNotNone(Rectangle.update.__doc__)
+        self.assertIsNotNone(Rectangle.to_dictionary.__doc__)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """Unit testing for Square class"""
 import unittest
-
+from models.base import Base
+from models.square import Square
+from unittest.mock import patch
+from io import StringIO
 
 class TestSquare(unittest.TestCase):
     """class for testing Square class"""
-    pass
+
+    def test_docstrings(self):
+        self.assertIsNotNone(__import__('models').rectangle.__doc__)
+        self.assertIsNotNone(Square.__doc__)
+        self.assertIsNotNone(Square.__init__.__doc__)
+        self.assertIsNotNone(Square.size.__doc__)
+        self.assertIsNotNone(Square.__str__.__doc__)
+        self.assertIsNotNone(Square.update.__doc__)
+        self.assertIsNotNone(Square.to_dictionary.__doc__)

44c5225ed145816982fb8ed3e46cf52f2e8a44b9
AaronManuel15 <aaronmanuel15@gmail.com>
2022-09-23 20:47:25 -0700

    method and class tests added

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -31,6 +31,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(Base._Base__nb_objects, 1)
         rect1 = Rectangle(5, 5, 0, 0, 1)
         self.assertEqual(rect1.id, 1)
+        del rect1
 
     def test_InputErrors(self):
         with self.assertRaises(TypeError):
@@ -55,3 +56,46 @@ class TestRectangle(unittest.TestCase):
             Rectangle(1, 1, -1, 1)
         with self.assertRaises(ValueError):
             Rectangle(1, 1, 1, -1)
+
+    def test_area(self):
+        rect2 = Rectangle(5, 5)
+        self.assertEqual(rect2.area(), 25)
+        del rect2
+
+    def test_display(self):
+        pass
+
+    def test_str(self):
+        rect3 = Rectangle(1, 1, 1, 1, 1)
+        self.assertEqual(rect3.__str__(), "[Rectangle] (1) 1/1 - 1/1")
+        del rect3
+
+    def test_update(self):
+        rect4 = Rectangle(10, 10, 10, 10, 10)
+        self.assertEqual(rect4.id, 10)
+        self.assertEqual(rect4.width, 10)
+        self.assertEqual(rect4.height, 10)
+        self.assertEqual(rect4.x, 10)
+        self.assertEqual(rect4.y, 10)
+        rect4.update(89, 2, 3, 4, 5)
+        self.assertEqual(rect4.id, 89)
+        self.assertEqual(rect4.width, 2)
+        self.assertEqual(rect4.height, 3)
+        self.assertEqual(rect4.x, 4)
+        self.assertEqual(rect4.y, 5)
+        rect4 = Rectangle(10, 10, 10, 10, 10)
+        rect4.update(height=1, width=1, x=2, id=89, y=3)
+        self.assertEqual(rect4.id, 89)
+        self.assertEqual(rect4.width, 1)
+        self.assertEqual(rect4.height, 1)
+        self.assertEqual(rect4.x, 2)
+        self.assertEqual(rect4.y, 3)
+        del rect4
+
+    def test_to_dictionary(self):
+        r1 = Rectangle(10, 2, 1, 9)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle(1, 1)
+        r2.update(**r1_dictionary)
+        self.assertIsInstance(r1_dictionary, dict)
+        self.assertIsNot(r2, r1)

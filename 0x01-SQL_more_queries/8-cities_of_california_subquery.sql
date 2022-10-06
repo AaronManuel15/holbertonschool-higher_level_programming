0eb09026fd369433afc401c21586788bc9807bba
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-06 14:14:54 -0700

    Changed what information to pull

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT * WHERE name = 'California'
-ORDER BY
+SELECT cities.id, cities.name WHERE cities.name = 'California'
+ORDER BY cities.id

91bd291973fc32b987152bc0d7a9ce887afbaa64
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-06 13:25:08 -0700

    edited the increment

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT AUTO INCREMENT NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );

88f7c84e00d70f7338f911894e0200c2059f078b
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-18 09:04:36 -0700

    Task 1 edit for case sensitivity

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,10 @@ def state_print_N():
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states")
     for row in cur.fetchall():
-        print(row)
+        if row[1][0] == "N":
+            print(row)
 
     cur.close()
     db.close()

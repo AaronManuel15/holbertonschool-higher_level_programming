43f0f7afd59baba816bf16b1a071720f9265c96b
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-18 13:23:02 -0700

    Updated to be better

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,9 @@ def state_print_N():
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE ASCII(name)=78 ORDER BY id")
     for row in cur.fetchall():
-        if row[1][0] == "N":
-            print(row)
+        print(row)
 
     cur.close()
     db.close()

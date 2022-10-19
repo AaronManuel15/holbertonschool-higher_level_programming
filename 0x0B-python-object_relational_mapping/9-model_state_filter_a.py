444a47119adcadc12e68d12074eb123499ce6c65
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-19 14:56:37 -0700

    Task 9

---
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Project 0x0B. Python - Object-relational mapping
+    Task 7
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    """ Prints all State objects from database
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for id, name in session.query(State.id, State.
+                                  name).filter(State.name.contains("a")):
+        print("{}: {}".format(id, name))

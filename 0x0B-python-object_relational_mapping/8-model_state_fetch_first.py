c42f911485897fece9a6670b572585e2ab20deb6
AaronManuel15 <aaronmanuel15@gmail.com>
2022-10-19 14:35:12 -0700

    Task 8

---
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Project 0x0B. Python - Object-relational mapping
+    Task 7
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    """ Prints all State objects from database
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    first_state = session.query(State.id, State.name).first()
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")

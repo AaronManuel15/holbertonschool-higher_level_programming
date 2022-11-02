b72123b3356ed9f3c52e7c7e5036486d119b29b7
AaronManuel15 <aaronmanuel15@gmail.com>
2022-11-01 18:58:37 -0700

    Task 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Task 3
+"""
+
+if __name__ == "__main__":
+    from urllib import request, parse, error
+    from sys import argv
+
+    try:
+        with request.urlopen(argv[1]) as resp:
+            resp = resp.read().decode('utf8')
+
+    except error.HTTPError as uhoh:
+        print("Error code: {}".format(uhoh.code))

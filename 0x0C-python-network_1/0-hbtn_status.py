f2bb212a2cb4bf0e1c468def7d5b30f2c22aca61
AaronManuel15 <aaronmanuel15@gmail.com>
2022-11-01 17:52:51 -0700

    Task 0

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Task 0
+"""
+
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen("https://intranet.hbtn.io/status") as resp:
+        resp = resp.read()
+        print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(resp), resp, resp.decode("utf8")))

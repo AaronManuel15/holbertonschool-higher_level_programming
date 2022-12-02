9ca1c57e5a1ccf7f335e3efa874b37b96be02ac5
AaronManuel15 <aaronmanuel15@gmail.com>
2022-12-01 18:39:12 -0800

    changed the process call

--- 0x00-javascript-warm_up/3-value_argument.js
+++ 0x00-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-let response;
 if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
-  response = process.argv.slice(2);
-  console.log(response[0]);
+  console.log(process.argv[2]);
 }

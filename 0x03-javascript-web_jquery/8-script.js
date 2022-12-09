ad51ae81652020ab32d30859540233f964a4411d
AaronManuel15 <aaronmanuel15@gmail.com>
2022-12-08 16:38:35 -0800

    closed out li tag

--- 0x03-javascript-web_jquery/8-script.js
+++ 0x03-javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 $(function () {
   $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
     $.each(data.results, function (i, film) {
-      $('#list_movies').append('<li>' + film.title + '<li>');
+      $('#list_movies').append('<li>' + film.title + '</li>');
     });
   });
 });
